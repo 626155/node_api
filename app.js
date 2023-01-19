757611ff03fdf591a3d5ce80f12278fa7522b068
DELL <pravesh@ubitechsolutions.com>
2023-01-20 01:27:47 +0530

    Signed-off-by: DELL <pravesh@ubitechsolutions.com>

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const Joi = require('@hapi/joi');
+const movies = require('./movies');
+const mysql = require('mysql');
+
+app.use(express.json());
+
+
+app.use('/abc', movies);
+
+
+app.get('/', (req,res) => {
+    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
+});
+
+
+const port = process.env.PORT || '5000';
+app.listen(port, () => console.log(`Server started on Port ${port}`));
